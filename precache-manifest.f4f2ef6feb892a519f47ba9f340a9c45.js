dbef8b86cfef9425a239f695230fcf25346234e5
m.shestakov <m.shestakov@itransition.com>
2020-06-28 03:49:50 +0300

    Updates

---
+++ precache-manifest.f4f2ef6feb892a519f47ba9f340a9c45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34289e97b96a9df24f5c33a320cc1fd4",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "a344af60100bb30e05cf",
+    "url": "/react-example/static/css/main.c2e92c89.chunk.css"
+  },
+  {
+    "revision": "a4990206715dc3ef60d1",
+    "url": "/react-example/static/js/2.34379a20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-example/static/js/2.34379a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f852845165aa0e82e1",
+    "url": "/react-example/static/js/3.833a8c96.chunk.js"
+  },
+  {
+    "revision": "a344af60100bb30e05cf",
+    "url": "/react-example/static/js/main.47d8d701.chunk.js"
+  },
+  {
+    "revision": "fb6884bfd39afe3dee31",
+    "url": "/react-example/static/js/runtime-main.c03c3fc3.js"
+  }
+]);
